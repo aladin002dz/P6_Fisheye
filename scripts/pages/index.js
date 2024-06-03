f3088ae91111f9c4fbc6b0483d4be50a7ee21622
Victor Quertelet <victor.quertelet@gmail.com>
2024-06-03 15:07:33 -0400

    Importation des datas et intégration de la page d'accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,12 @@
+    // Récupération des datas des photographes
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        const response = await fetch('data/photographers.json');
+        const data = await response.json();
+        
+        return (data.photographers)
     }
 
+    // Créer une section et y intègre chaque card des photographes
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
@@ -36,9 +17,9 @@
         });
     }
 
+    // Exécute l'intégration des datas dans le DOM
     async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
         displayData(photographers);
     }
     
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,44 @@
+// Intègre les données des photographes dans le DOM en créant une card
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        const link = document.createElement( 'a' );
+        link.href = './photographer.html';
+        link.setAttribute("aria-label", `Lien vers la page de ${name}`);
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Photo de ${name}`);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
+        h2.setAttribute("aria-label", "Nom du photographe");
+
+        const pCountry = document.createElement ( 'p' );
+        pCountry.textContent = `${city}, ${country}`;
+        pCountry.setAttribute("aria-label", "Localisation du photographe");
+
+        const pTagline = document.createElement ( 'p' );
+        pTagline.textContent = tagline;
+        pTagline.setAttribute("aria-label", "Slogan du photographe");
+
+        const pPrice = document.createElement ( 'p' );
+        pPrice.textContent = `${price}€/jour`;
+        pPrice.setAttribute("aria-label", "Le prix");
+        
+        article.appendChild(link);
+        link.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pCountry);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, city, country, tagline, price, picture, getUserCardDOM }
 }
+
