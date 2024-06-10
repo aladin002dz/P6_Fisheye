bf7972a405ee7b3a87d7e3e90e829cd81f3d187e
Victor Quertelet <victor.quertelet@gmail.com>
2024-06-10 10:57:02 -0400

    Ajout de la fonction like sur les photos

--- scripts/templates/photographerPage.js
+++ scripts/templates/photographerPage.js
@@ -1,6 +1,5 @@
 // Affichage des datas du photographe
 function displayPhotographerData(photographerData) {
-    
     // Header
     const photographHeader = document.querySelector('.photograph-header');
     const textContent = document.createElement( 'div' );
@@ -45,12 +44,16 @@ function displayPhotographerData(photographerData) {
     
     // Main
     const main = document.querySelector('main');
+    const photographInsert = document.createElement( 'div' );
+    photographInsert.classList.add('photograph-insert');
     const price = document.createElement( 'p' );
     price.textContent = `${photographerData.price}€/jour`;
     price.classList.add('photograph-price');
 
-    main.appendChild(price);
+    main.appendChild(photographInsert);
+    photographInsert.appendChild(price);
 }
+
 
 // Créer une nouvelle instance d'un objet Image ou Video. 
 function MediaFactory (media) {
@@ -74,28 +77,95 @@ function MediaFactory (media) {
 // Création de la carte pour chaque média
 function cardDom (media) {
     const mediaElement = new MediaFactory(media);
-    const card = `<div class="card">${mediaElement.outerHTML}
+    const card = 
+    `<div class="card">${mediaElement.outerHTML}
     <div class="card-content">
     <h2>${media.title}</h2>
     <div class="button-container">
-        <p>${media.likes}</p>
-        <button aria-label="like" class="like-btn">
+        <p class="like-card-content" data-id="${media.id}">${media.likes}</p>
+        <button aria-label="like" class="like-btn" data-id="${media.id}">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
         </button>
-    </div>
-    </div>
-    </div>`
-    return card;
+        </div>
+        </div>
+        </div>`
+        return card;
 }
+
+// Calcul du total des likes
+function getTotalLikes(photographerMedia) {
+    let totalLikes = 0;
+    for (let media of photographerMedia) {
+        totalLikes += media.likes;
+    }
+    return totalLikes;
+}
+
+// Fonction d'ajout des likes au clic
+function addLikeEventListeners(photographerMedia) {
+    photographerMedia.forEach((media) => {
+        const likeButton = document.querySelector(`.like-btn[data-id="${media.id}"]`);
+        const likeButtonSvg = likeButton.querySelector('svg');
+        likeButton.dataset.liked = 'false';
+        likeButton.addEventListener('click', () => {
+            // Vérifie si le like est true ou false
+            const isLiked = likeButton.dataset.liked === 'true';
+            likeButton.dataset.liked = isLiked ? 'false' : 'true';
+            media.likes += isLiked ? -1 : 1;
+            if (likeButton.dataset.liked === 'true') {
+                likeButtonSvg.classList.add('liked');
+            } else {
+                likeButtonSvg.classList.remove('liked');
+            }
+
+            // Mis à jour des likes de la card
+            const likeDisplay = document.querySelector(`.like-card-content[data-id="${media.id}"]`);
+            likeDisplay.textContent = media.likes;
+        
+            // Mis à jour du total des likes
+            let totalLikes = getTotalLikes(photographerMedia);
+            const likesElement = document.querySelector(".likes-element");
+            likesElement.textContent = totalLikes;
+        });
+    });
+}
+        
 
 // Affichage des médias du photographe
 function displayPhotographerMedia(photographerMedia) {
+    
     const gallery = document.querySelector('.gallery');
     gallery.innerHTML = '';
-
+    
     photographerMedia.forEach(media => {
         const cardElement = cardDom(media);
-            gallery.insertAdjacentHTML("beforeend", cardElement);
-        });
+        gallery.insertAdjacentHTML("beforeend", cardElement);
+    });
+    
+    // Ajout du total des likes
+    let totalLikes = getTotalLikes(photographerMedia);
+    const photographInsert = document.querySelector('.photograph-insert')
+    
+    // Vérifie si likesContent existe déjà pour éviter les doublons
+    let likesContent = photographInsert.querySelector('.likes-content');
+    if (!likesContent) {
+        likesContent = document.createElement('div');
+        likesContent.classList.add('likes-content');
+        photographInsert.appendChild(likesContent);
+    } else {
+        likesContent.innerHTML = '';
+    }
+
+    // Ajout du total des likes et du svg en html
+    const likesElement = document.createElement('p');
+    likesElement.classList.add('likes-element');
+    const svgElement = document.createElement('svg'); 
+    svgElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>`
+    likesElement.textContent = `${totalLikes}`;
+
+    likesContent.appendChild(likesElement);
+    likesContent.appendChild(svgElement);
 }
+
+
 
